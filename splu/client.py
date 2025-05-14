9e91d7a76b5100c2711cd6c63a35466771ba817f
Harrison Fang <8905434+harrisonfang@users.noreply.github.com>
2025-05-14 02:08:08 +0000

    docstring and visibility

--- splu/client.py
+++ splu/client.py
@@ -4,6 +4,14 @@ import spotipy
 
 
 def get(*, scopes: list[str]) -> spotipy.Spotify:
+    """
+    Initialize an authenticated spotipy client in headless mode (open_browser=False).
+
+    In addition to the typical environment variables, we also add support for SPOTIPY_CACHE_PATH. This allows the caller
+    to override the default cache location. If the cache exists, a client can be initialized automatically using the
+    cache's refresh_token without manual authentication. However, requesting a new client with different scopes will
+    invalidate the cache and require authentication again.
+    """
     return spotipy.Spotify(
         auth_manager=spotipy.oauth2.SpotifyOAuth(
             cache_handler=spotipy.oauth2.CacheFileHandler(
